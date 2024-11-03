a08f3ec5eaabf26d8389481199c246f01e35b8a9
owner <owner1147@gmail.com>
2024-11-03 18:53:40 +0200

    lisling update

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import { authMiddleware } from './src/middleware/authMiddleware.js';
 import authRouter from './src/routes/auth.router.js';
 import creatingListingsRouter from './src/routes/Listings/creatingListings.router.js';
 import listingsUserRouter from './src/routes/Listings/listingsUser.router.js';
+import updateUserListingRouter from './src/routes/Listings/updateUserListing.router.js';
 dotenv.config(); 
 
 const app = express();
@@ -13,11 +14,11 @@ const app = express();
 const PORT = process.env.SERVER_PORT || 3000
 
 app.use(cors())
-
 app.use(express.json())
-app.use("/auth", authRouter)
+app.use('/auth', authRouter)
 app.use('/create-listings', authMiddleware, creatingListingsRouter);
-app.use('/user-listings', authMiddleware, listingsUserRouter)
+app.use('/user-listings', authMiddleware, listingsUserRouter);
+app.use('/update-listing/:id', updateUserListingRouter);
 const start = async () => {
   await prisma.$connect();
 
