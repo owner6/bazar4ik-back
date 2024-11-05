dc81a6e4fec7c9eeb08d49352a6a22cbf66f1fb3
owner <owner1147@gmail.com>
2024-11-05 15:10:53 +0200

    fix errors getting data from req.body

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ import express from 'express'
 import prisma from './src/lib/prisma.js'
 import dotenv from 'dotenv';
 import cors from 'cors';
-import { authMiddleware } from './src/middleware/authMiddleware.js';
-import authRouter from './src/routes/auth.router.js';
-import creatingListingsRouter from './src/routes/Listings/creatingListings.router.js';
-import listingsUserRouter from './src/routes/Listings/listingsUser.router.js';
-import updateUserListingRouter from './src/routes/Listings/updateUserListing.router.js';
-dotenv.config(); 
+
+import router from './src/routes/router.js';
+
+dotenv.config();
 
 const app = express();
 
@@ -15,10 +13,9 @@ const PORT = process.env.SERVER_PORT || 3000
 
 app.use(cors())
 app.use(express.json())
-app.use('/auth', authRouter)
-app.use('/create-listings', authMiddleware, creatingListingsRouter);
-app.use('/user-listings', authMiddleware, listingsUserRouter);
-app.use('/update-listing/:id', updateUserListingRouter);
+
+app.use('/', router)
+
 const start = async () => {
   await prisma.$connect();
 
---
+++ src/routes/router.js
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+
+import { authMiddleware } from "../middleware/auth.middleware.js";
+import authController from "../controllers/auth.controller.js";
+import { createListingAction, getUserListingsAction, updateUserListingAction } from "../controllers/listings.controller.js";
+
+const router = Router();
+
+// Auth routes
+router.post('/auth/registration', authController.registration);
+router.post('/auth/login', authController.login);
+
+// Users routes
+router.get('/users', authMiddleware, authController.getUsers)
+
+// Listings routes
+router.post('/listings', authMiddleware, createListingAction);
+router.get('/listings', authMiddleware, getUserListingsAction);
+router.get('/listings/:id', () => {})
+router.put('/listings/:id', authMiddleware, updateUserListingAction)
+router.delete('/listings/:id', () => {})
+
+export default router;
