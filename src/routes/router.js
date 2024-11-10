d9f795cca63721a0cafdc45c8f22511ca0652d6c
owner <owner1147@gmail.com>
2024-11-10 22:18:01 +0200

    коміт

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 import { authMiddleware } from "../middleware/auth.middleware.js";
 import authController from "../controllers/auth.controller.js";
-import { createListingAction, getUserListingsAction, updateUserListingAction } from "../controllers/listings.controller.js";
+import { createListingAction, getUserListingsAction, updateUserListingAction, deleteListingAction } from "../controllers/listings.controller.js";
 
 const router = Router();
 
@@ -16,8 +16,8 @@ router.get('/users', authMiddleware, authController.getUsers)
 // Listings routes
 router.post('/listings', authMiddleware, createListingAction);
 router.get('/listings', authMiddleware, getUserListingsAction);
-router.get('/listings/:id', () => {})
-router.put('/listings/:id', authMiddleware, updateUserListingAction)
-router.delete('/listings/:id', () => {})
+router.get('/listings/:id', () => {});
+router.put('/listings/:id', authMiddleware, updateUserListingAction);
+router.delete('/listings/:id', authMiddleware, deleteListingAction);
 
 export default router;
